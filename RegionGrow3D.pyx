ea807f8a8f86ddbe890fcbda4643172863fc2de3
Kaan Yilmaz <kaan-yilmaz@outlook.com.tr>
2018-10-01 14:54:12 +0200

    Readability fix

--- RegionGrow3D.pyx
+++ RegionGrow3D.pyx
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.debug)
 
 cdef class RegionGrow3D:
     
-    cdef int[:,:,:] slices
+    cdef int[:,:,:] images
     cdef int[:,:,:] masks
     cdef int[:,:,:] tracheaMask
     cdef int upperThreshold
@@ -22,11 +22,11 @@ cdef class RegionGrow3D:
     cdef neighborMode
     cdef queue
     
-    def __cinit__(self, int[:,:,:] slices, int[:,:,:] masks, 
+    def __cinit__(self, int[:,:,:] images, int[:,:,:] masks, 
                   int upperThreshold, int lowerThreshold, neighborMode):
-        self.slices = slices
+        self.images = images
         self.masks = masks
-        self.tracheaMask = np.zeros_like(self.slices)
+        self.outputMask = np.zeros_like(self.slices)
         self.upperThreshold = upperThreshold
         self.lowerThreshold = lowerThreshold
         self.neighborMode = neighborMode
@@ -38,7 +38,7 @@ cdef class RegionGrow3D:
         cdef int neighbors[26][3]
         newItem = seed
         
-        self.tracheaMask[newItem[0], newItem[1], newItem[2]] = 1
+        self.outputMask[newItem[0], newItem[1], newItem[2]] = 1
         self.queue.append((seed[0], seed[1], seed[2]))
         
         while len(self.queue) != 0:
@@ -62,14 +62,16 @@ cdef class RegionGrow3D:
                 self.checkNeighbour(newItem[0], newItem[1]+1, newItem[2])
                 self.checkNeighbour(newItem[0]-1, newItem[1], newItem[2])
                 self.checkNeighbour(newItem[0]+1, newItem[1], newItem[2])
-        return self.tracheaMask
+        return self.outputMask
         
     cdef checkNeighbour(self, int z, int y, int x):
         cdef int intensity
-        if x < 513 and y < 513 and z < 513 and x > -1 and y > -1 and z > -1 and self.masks[z,y,x] == 1:
-            intensity = self.slices[z, y, x]
-            if self.isIntensityAcceptable(intensity) and self.tracheaMask[z,y,x] == 0:
-                self.tracheaMask[z,y,x] = 1
+        if (x < 513 and y < 513 and z < 513 
+            and x > -1 and y > -1 and z > -1 
+            and self.masks[z,y,x] == 1):
+            intensity = self.images[z, y, x]
+            if self.isIntensityAcceptable(intensity) and self.outputMask[z,y,x] == 0:
+                self.outputMask[z,y,x] = 1
                 self.queue.append((z, y, x))
     
     cdef isIntensityAcceptable(self, int intensity):
